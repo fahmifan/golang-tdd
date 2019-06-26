3b04e04629107cec2dbd6ed4296121833bb120b2
miun173 <fahmifan173@gmail.com>
2019-06-26 07:38:38 +0700

    feat: print more info when error

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -35,7 +35,7 @@ func TestArea(t *testing.T) {
 		t.Helper()
 		got := shape.Area()
 		if got != want {
-			t.Errorf("got %v, want %v", got, want)
+			t.Errorf("%#v got %.2f want %.2f", shape, got, want)
 		}
 	}
 
