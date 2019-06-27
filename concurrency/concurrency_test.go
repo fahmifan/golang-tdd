73bc824ed092eb64620ddf2d347b5a6fd67a814f
miun173 <fahmifan173@gmail.com>
2019-06-27 09:09:39 +0700

    feat: concurrency

---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,55 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+var invalidURL = "waat://furhurterwe.geds"
+
+func mockWebsiteChecker(url string) bool {
+	if url == invalidURL {
+		return false
+	}
+
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("got: %v, \nwant: %v", got, want)
+	}
+}
+
+// pause for 20 milis for every request
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsite(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
